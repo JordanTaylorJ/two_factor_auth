eb47df8b7c84d3991041cb2e04a5caed77dd5757
JordanJoseph <jordant.joseph@outlook.com>
2024-04-18 12:55:59 -0600

    add session cookies middleware to config/application.rb, include in application_controller in order for controllers to inherit functionality, define signup route and associated create action in routes and users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id 
+            render json: user, status: :ok
+        else 
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entitiy 
+        end
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
+    end
+
 end
