e9f3201c6d9e0d5db4131d6b447e4c14f58c83df
JordanJoseph <jordant.joseph@outlook.com>
2024-04-21 15:17:07 -0600

    define auth route and coorelated create method in users_controller, generate sessions_controller for login/logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def show 
+        current_user = User.find(session[:user_id])
+        render json: current_user, status: :ok 
+    end
+
     def create
         user = User.create(user_params)
         if user.valid?
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   #resources :users
   
   post "/signup", to: "users#create"
+  get "/auth", to "users#show"
+  
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
