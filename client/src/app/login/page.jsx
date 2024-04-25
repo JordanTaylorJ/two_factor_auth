ff5692cf28727b789b740ce4397a9033b9d5e7f7
JordanJoseph <jordant.joseph@outlook.com>
2024-04-24 19:13:31 -0600

    create login and home pages with next js App Router conventions

---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return( 
+        <main className="min-h-screen">
+        <h1>Login</h1>
+        </main>
+    )
+}
+
+export default Login;
