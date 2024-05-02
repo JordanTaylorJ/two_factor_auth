c1e3342a83bb8d919afc56f1b712a394671b0656
JordanJoseph <jordant.joseph@outlook.com>
2024-05-02 12:25:36 -0600

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  #resources :users
+    #resources :users
   
-  post '/signup', to: 'users#create'
-  get '/auth', to: 'users#show'
+    namespace :api do:
 
+        post '/signup', to: 'users#create'
+        get '/auth', to: 'users#show'
+        post '/login', to: 'sessions#create'
+        delete '/logout', to: 'sessions#destroy'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    end 
+  
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    # root "articles#index"
 end
